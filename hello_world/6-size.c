499758425d2908607f8ff77babbd0f1c717bfe86
ispdls <is.pdls90@gmail.com>
2023-10-13 14:59:50 -0400

    zise

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,6 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n
-Size of an int: 4 byte(s)\n
-Size of a long int: 4 byte(s\n)
-Size of a long long int: 8 byte(s)\n
-Size of a float: 4 byte(s)\n
-julien@ubuntu:~/c/$ ./size64\n
-Size of a char: 1 byte(s)\n
-Size of an int: 4 byte(s)\n
-Size of a long int: 8 byte(s)\n
-Size of a long long int: 8 byte(s)\n
-Size of a float: 4 byte(s)");
-return (0);	
+printf(type32, type64);
+return (0);
 }

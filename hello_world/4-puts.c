a6ba2d92bccdb8086be4aa6ab36fdc509e66af5f
ispdls <is.pdls90@gmail.com>
2023-10-13 14:01:38 -0400

    putc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /*
  * main - Entry point
  *  Description: 'print'
- * Return: Always 0 (Success)
  */
 int main(void)
 {
-putc("Programming is like building a multilingual puzzle");
+	putc("Programming is like building a multilingual puzzle");
 return ();
 }

b63929a8bfa266d541ff292a9ffd43f6e18a71a3
ispdls <is.pdls90@gmail.com>
2023-10-13 14:53:31 -0400

    zise

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 4 byte(s\n)
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n
+julien@ubuntu:~/c/$ ./size64\n
+Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 8 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)");
+return (0);	
+}

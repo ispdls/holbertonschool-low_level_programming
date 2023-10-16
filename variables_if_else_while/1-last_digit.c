44b2ad2b413e50c71d44fce3b9c2473982128494
ispdls <is.pdls90@gmail.com>
2023-10-16 14:12:40 -0400

    greater of less

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ if (x > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, x);
 }
-else if (x == 0)
+if (x == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, x);
 }
-else if (x < 6 && x != 0)
+if (x < 6 && x != 0)
 {
 printf("Last digit of %d is %d and is less than 6 end not 0\n", n, x);
 }

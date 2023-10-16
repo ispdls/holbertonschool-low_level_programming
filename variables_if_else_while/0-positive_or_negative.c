ba0e4423d1f0b2a7a9b7553f5227af6717370db9
ispdls <is.pdls90@gmail.com>
2023-10-16 10:57:45 -0400

    va

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,16 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0);
 {
 	printf("is positive");
 }
-if (n == 0);
+if else(n == 0);
 {
 	printf("is zero");
 }
-if (n < 0);
+if else(n < 0);
 {
 	printf("is negative");
 }

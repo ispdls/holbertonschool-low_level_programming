e2cee064c13f630804052046c741fcf5ab385136
ispdls <is.pdls90@gmail.com>
2023-10-16 14:02:18 -0400

    greater of less

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,23 +7,25 @@
  */
 int main(void)
 {
-	int n;
-	int x;
+int n;
+int x;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-	if (n > 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, x);
-	}
-	else if (n == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, x);
-	}
-	else if (n < 6)
-	{
-	printf("Last digit of %d is %d and is less than 6 end not 0\n", n, x);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+x = n % 10;
+if (x > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, x);
 }
+else if (x == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, x);
+}
+else if (x < 6 && x > 1)
+{
+printf("Last digit of %d is %d and is less than 6 end not 0\n", n, x);
+}
+return (0);
+}
+
+

564acb2f5ad0dc2f310b427eee61c61384a82a62
ispdls <is.pdls90@gmail.com>
2023-10-17 13:54:09 -0400

    print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+*
+
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: alway 0
+ */
+void print_alphabet_x10(void)
+{
+
+char c;
+int con = 0;
+
+while (con < 10)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
+

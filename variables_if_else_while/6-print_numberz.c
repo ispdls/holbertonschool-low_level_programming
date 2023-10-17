42caaae19478e6c8ab3a645c17d16b862ec38f39
ispdls <is.pdls90@gmail.com>
2023-10-16 22:20:05 -0400

    print_number we putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: away
+ */
+int main(void)
+{
+for (int num = 0; num < 10 ; num++)
+{
+putchar('0' + num);
+}
+putchar('\n');
+return (0);
+}
+

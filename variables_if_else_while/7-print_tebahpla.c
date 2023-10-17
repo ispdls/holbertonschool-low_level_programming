5b2650b69af5d83c0de2fc14bac47166a90bb23b
ispdls <is.pdls90@gmail.com>
2023-10-16 22:30:40 -0400

    print the aphabet in reveser

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: away
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar (c);
+}
+putchar('\n');
+return (0);
+}
+

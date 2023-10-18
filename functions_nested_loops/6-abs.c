ce6edd145a99a69700a03c634f4482cf50f60258
ispdls <is.pdls90@gmail.com>
2023-10-17 22:16:51 -0400

    computes the absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer.
+ *
+ * Return: alway 0
+ */
+int _abs(int n);
+{
+if (n < 0)
+{
+int as;
+as = n * -1;
+return(as)
+}
+return(0)
+}
+

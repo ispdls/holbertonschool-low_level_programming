f601cbcb169f477f92358fb9f06c382f2b0f6895
ispdls <is.pdls90@gmail.com>
2023-10-18 19:23:51 -0400

    print the table from 0 to 9.

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*times_table - print the table from 0 t0 9.
+* Return: alway 0
+*/
+void times_table(void)
+{
+int n1;
+int n2;
+int n3;
+
+for (n1 = 0 ; n1 <= 9 ; n1++)
+{
+for (n2 = 0 ; n2 <= 9 ; n2++)
+{
+n3 = n1 * n2;
+_putchar(n3 / 10 + '0');
+_putchar(n3 % 10 + '0');
+_putchar(44);
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+
+

ce46ecb799ba94e5c720fd4d259cf3cc1873b4e4
ispdls <is.pdls90@gmail.com>
2023-10-17 22:30:32 -0400

    the last number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit - print the last number
+ *@n: the number
+ * Return: alway 0
+ */
+int print_last_digit(int n)
+{
+int lastnum;
+lastnum = n % 10;
+return (lastnum);
+}
+

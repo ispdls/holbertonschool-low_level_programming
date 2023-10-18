0b4350061e0b0ad88ba6374f207e979a18ca9823
ispdls <is.pdls90@gmail.com>
2023-10-17 20:15:48 -0400

    print the sing of number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_sign - print the sign of number.
+ *@n: the return value
+ * Return: 1.
+ */
+int print_sign(int n)
+{
+if (n >= 48 && n <= 57)
+{
+return (1);
+}
+return (0);
+}
+

e83699d0e242006f6653d7293d2176ecb2ef8721
ispdls <is.pdls90@gmail.com>
2023-10-18 23:47:44 -0400

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_to_98 - qway 98
+*@n: the return value
+* Return: 1.
+*/
+void print_to_98(int n)
+{
+int c;
+if (n == 98)
+{
+printf("%d", n);
+}
+if (n < 98)
+{
+for (c = n ; c <= 98 ; c++)
+{
+printf("%d", c);
+if (c < 89)
+{
+printf(", ");
+}
+}
+}
+if (n > 98)
+{
+for (c = n ; c >= 98 ; c--)
+{
+printf("%d", c);
+if (c > 98)
+{
+printf(", ");
+}
+}
+}
+}
+

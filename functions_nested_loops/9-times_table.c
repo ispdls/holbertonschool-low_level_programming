679081ee88a7ade5cdd24a3f089edd854cb39b38
ispdls <is.pdls90@gmail.com>
2023-10-19 10:04:48 -0400

    print table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,11 @@ for (n1 = 0 ; n1 <= 9 ; n1++)
 for (n2 = 0 ; n2 <= 9 ; n2++)
 {
 n3 = n1 * n2;
-if (n3 < 10)
+if (n2 < 0);
+{
+_putchar('0');
+}
+if (n3 <= 9)
 {
 _putchar(44);
 _putchar(' ');
@@ -23,17 +27,12 @@ _putchar(n3 + '0');
 }
 if (n3 > 9)
 {
-_putchar(n3 / 10 + '0');
-_putchar(n3 % 10 + '0');
-if (n2 < 9)
-{
 _putchar(44);
 _putchar(' ');
-}
+_putchar(n3 / 10 + '0');
+_putchar(n3 % 10 + '0');
 }
 }
 _putchar('\n');
 }
 }
-
-

ef5541a76827241a37835cfea34d37f8b4c31d08
ispdls <is.pdls90@gmail.com>
2023-10-18 22:59:35 -0400

    add two number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+/**
+*add - add two number
+*@n1: add number
+*@n2: add number
+* Return: 1.
+*/
+int add(int n1, int n2)
+{
+int res = 0;
+
+res = n1 + n2;
+return (res);
+}
+

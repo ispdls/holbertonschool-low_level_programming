4258688ca0195473d95cb111d8d82c2f2c5b39ab
ispdls <is.pdls90@gmail.com>
2023-10-19 20:51:22 -0400

    check for upper case lattle

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - check the alphabet
+*@c: return 1 o 0
+*Return: alway
+*/
+int _isupper(int c)
+{
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		return (1);
+	}
+return (0);
+}

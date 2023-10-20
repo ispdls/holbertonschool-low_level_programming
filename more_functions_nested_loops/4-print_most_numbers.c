fb22bddd73814a160519ab0f47a71f23e40ea3ee
ispdls <is.pdls90@gmail.com>
2023-10-19 22:01:23 -0400

    print a line of numbers from 0 to 9, do not print '2 and 4'

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_most_numbers - print number from 0 to 9 do not print 2 4.
+ *Return: alway
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;
+
+		}
+
+
+	}
+
+}

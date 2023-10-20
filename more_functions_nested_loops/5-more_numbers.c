288fbef0da2116a959fa2e114816606154d5da3e
ispdls <is.pdls90@gmail.com>
2023-10-19 22:44:52 -0400

    print a line of numbers from 0 to 9 on table

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *Return: alway
+ */
+void more_numbers(void)
+{
+	int n1 = 0;
+	int n2;
+
+	while (n1 <= 10)
+	{
+		for (n2 = 0 ; n2 < 15 ; n2++)
+		{
+			if (n2 < 10)
+			{
+				_putchar(n2 + '0');
+
+				else
+				{
+					_putchar(n2 / 10 + '0');
+					_putchar(n2 % 10 + '0');
+				}
+
+			}
+
+		}
+		_putchar('\n');
+		n1++;
+	}
+}

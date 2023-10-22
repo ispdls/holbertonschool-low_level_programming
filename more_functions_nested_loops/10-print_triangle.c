93d0f5736acd4deb924a00670600220e5064d152
ispdls <is.pdls90@gmail.com>
2023-10-22 19:12:36 -0400

    print triangule

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_triangle - print a trangle we ##.
+ *@size: retur value
+ *Return: alway
+ */
+void print_triangle(int size)
+{
+	int vline;
+	int hline;
+
+	if (size > 0)
+	{
+		for (vline = 1 ; vline <= size ; vline++)
+		{
+			for (hline = 1 ; hline <= size - 1 ; hline++)
+			{
+				_putchar(' ');
+			}
+			for (hline = 1 ; hline <= vline ; hline++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

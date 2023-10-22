25c1d27145fbfa0da7b5bacdd467a564067bba4a
ispdls <is.pdls90@gmail.com>
2023-10-22 17:18:00 -0400

    print 1 to 100 FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - print 1 to 100.
+*Return: alway
+*/
+int main(void)
+{
+	int con;
+
+	for (con = 0 ; con <= 100 ; con++)
+	{
+		if (con == 0)
+		{
+			printf("%d", con);
+		}
+		else if (con % 3 == 0 && con % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (con % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (con % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", con);
+			printf(" ");
+		}
+		printf("\n");
+	}
+}

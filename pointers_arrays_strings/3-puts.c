ac945b54aa975f955e2bcff6c5cc9a07ea1ada8e
ispdls <is.pdls90@gmail.com>
2023-10-23 13:51:48 -0400

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: return value
+ *Return: alway
+ */
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+	len++;
+	}
+
+}

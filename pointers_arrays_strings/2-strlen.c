6cae22d71f8585cd677338d6d72347321fc85da5
ispdls <is.pdls90@gmail.com>
2023-10-23 11:30:34 -0400

    len

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ *_strlen - the length of a string.
+ *@s: return value
+ *Return: alway
+ */
+int _strlen(char *s)
+{
+	char len;
+
+	len = strlen(s);
+	return (len);
+}

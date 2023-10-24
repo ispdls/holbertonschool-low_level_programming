02c7d2db7a91e8a213823cdf9743b3a3246b9a2e
ispdls <is.pdls90@gmail.com>
2023-10-23 23:01:21 -0400

    function that reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ *rev_string - function the reverse a string.
+ *@s: return value
+ *Return: alway
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	int firstl = 0;
+	int lastl = len - 1;
+
+	while (firstl < lastl)
+	{
+		char temp = s[firstl];
+
+		s[firstl] = s[lastl];
+		s[lastl] = temp;
+	}
+}

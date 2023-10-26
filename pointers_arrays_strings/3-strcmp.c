1eab2576de6f3236d81a9646527a010e6d8497d0
ispdls <is.pdls90@gmail.com>
2023-10-26 12:09:49 -0400

    compares two string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strcmp - compares two string
+ *@si: value to compare
+ *@s2: value to compare
+ *Return: the value compare of s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	for (index = o ; s1[index] != '\0' && s2[index] != '\0' ; index++)
+	{
+		if (s1[index] != s2[index])
+			return (s1[index] - s2[index];
+	}
+}

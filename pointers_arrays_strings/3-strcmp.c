b71b8db97600880f2c67c7337ab3743a0d28e461
ispdls <is.pdls90@gmail.com>
2023-10-26 12:11:34 -0400

    compares two string

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int index;
 
-	for (index = o ; s1[index] != '\0' && s2[index] != '\0' ; index++)
+	for (index = 0 ; s1[index] != '\0' && s2[index] != '\0' ; index++)
 	{
 		if (s1[index] != s2[index])
-			return (s1[index] - s2[index];
+			return (s1[index] - s2[index]);
 	}
 }

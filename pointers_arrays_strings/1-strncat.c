3635fb45b33a2f55172eadb88218ac0b84201b9f
ispdls <is.pdls90@gmail.com>
2023-10-26 10:17:40 -0400

    concatenates two strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		j++;
 	}
-	for (len = 0 ; len < n && src[len] != '\0' ; len++, len++)
+	for (len = 0 ; len < n && src[len] != '\0' ; len++, j++)
 	{
 		dest[j] = src[len];
 	}

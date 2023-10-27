370eaf61665d601daa5f28c6be2c92fa534d64c0
ispdls <is.pdls90@gmail.com>
2023-10-27 10:03:03 -0400

    capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * *cap_string - capitalizes all words of a string
+ * @str: return value
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int index;
+	char temp[50];
+
+	for (index = 0 ; str[index] != '\0' ; index++)
+	{
+		if (str[index] >= 'a' || str[index] <= 'z')
+		{
+			temp[index] = str[index - 1];
+			if (temp[index] == '\t' || temp[index] == '\n')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (temp[index] == '{' || temp[index] == '}')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (temp[index] == '(' || temp[index] == ')')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (temp[index] == ',' || temp[index] == '.' || temp[index] == ';')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (temp[index] == '?' || temp[index] == '!')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (temp[index] == ' ' || temp[index] == '"')
+			{
+				str[index] = str[index] - 32;
+			}
+			if (index == 0)
+			{
+				str[index] = str[index] - 32;
+			}
+		}
+	}
+	return (str);
+}

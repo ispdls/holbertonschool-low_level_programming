8208fc93f586c8e421a5ba5c81a1dd8dd8fe6afe
ispdls <is.pdls90@gmail.com>
2023-10-29 19:48:15 -0400

    searches a string for any of a set of bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_strpbrk - searchs a string for any of a set of bytes
+ * @s: the string 
+ * @accept: the bytes
+ * Return: NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}

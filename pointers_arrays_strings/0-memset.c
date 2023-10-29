cd8d549cb49e85057e37b502dea530c59af0afdf
ispdls <is.pdls90@gmail.com>
2023-10-29 01:04:54 -0400

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ *_memset - fills memory with a constant byte
+ *@s:pointer to str
+ *@b:constar byte
+ *@n:bum byte
+ *Return: str
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int index = 0;
+	unsigned int i = 0;
+	char *str = s;
+
+	while(i < n)
+	{
+		*s++ = b;
+		i++;
+	}
+	return (str)
+}

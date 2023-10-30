c2c8048b5491ec1ef36c4d01c5014c320693a36f
ispdls <is.pdls90@gmail.com>
2023-10-29 21:05:00 -0400

    locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack:string to search
+ * @needle: search
+ * Return:NULL o haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}

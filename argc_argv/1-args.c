c1e3821e04487fa039f56ca0bc0db35552e3eff8
ispdls <is.pdls90@gmail.com>
2023-11-02 17:14:21 -0400

    prints the number of arguments passed into it.

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point.
+ * @argc: command line arguments
+ * @argv:An array containing the program command line arguments
+ * Return: alway 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
+

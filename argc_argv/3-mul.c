9ff3898080c4ebab9b759478c60b32f5a9acd5c0
ispdls <is.pdls90@gmail.com>
2023-11-03 09:51:10 -0400

    multiplies two numbers.

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  multiplies two numbers.
+ * @argc: command line arguments
+ * @argv:An array containing the program command line arguments
+ * Return: alway 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mult, mult1, total;
+
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	for (i = 1; i < argc ; i++)
+	{
+		mult =  atoi(argv[1]);
+		mult1 = atoi(argv[2]);
+		total = mult * mult1;
+	}
+	printf("%d\n", total);
+	return (0);
+}

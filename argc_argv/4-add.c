f1487c8739d39c990d6ee92a34afff994e7a81f5
ispdls <is.pdls90@gmail.com>
2023-11-05 21:08:39 -0400

    add two numbes

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: command line arguments
+ * @argv:An array containing the program command line arguments
+ * Return: alway 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum, sum1, total;
+
+	for (i = 1; i < argc ; i++)
+	{
+		sum = atoi(argv[i]);
+		if (*argv[i] >= 'a' && *argv[i] <= 'z')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if  (sum >= 1)
+		{
+			sum1 = sum;
+			total = total + sum1;
+		}
+		else
+		{
+			printf("0\n");
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}

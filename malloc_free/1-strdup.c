00ba885a7125cd9797668b2f92806bac416c4600
ispdls <is.pdls90@gmail.com>
2023-11-12 16:19:43 -0400

    returns a pointer to a newly allocated space in memory,

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * char *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to copy
+ * Return: copy o NULL
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int i, len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		len++;}
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (cpy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		cpy[i] = str[i];
+	}
+	cpy[len] = '\0';
+	return (cpy);
+}

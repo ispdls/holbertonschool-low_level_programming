e6691a9af313f650939e49c664c1f4c57be451f0
ispdls <is.pdls90@gmail.com>
2023-11-15 09:53:22 -0400

    struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#ifndef "dog.h"
+#define "dog.h"
 /**
  * struct dog - struct dog
- * @name; name of the dos
+ * @name: name of the dos
  * @age: age of the dog
  * @owner: name of the owner
- * Return: null
  */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner
+char *name;
+float age;
+char *owner
 };

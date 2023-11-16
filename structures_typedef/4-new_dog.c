6400082bb33557491b2ffa67a2d540e12c057743
ispdls <is.pdls90@gmail.com>
2023-11-16 11:10:25 -0400

    function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * new_dog - function that prints a struct dog
+ * new_dog - function that copiesa struct dog
  * @name: name of the dog struct
  * @age: age struct
  * @owner: owner name
@@ -11,12 +11,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int namelen, ownerlen, i;
 
+	while (name[namelen++])
+		;
+	while (owner[ownerlen++])
+		;
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+
+	dog->name = malloc(namelen * sizeof(dog->name));
+	if (dog == NULL)
+		return (NULL);
+	for (i = 0 ; i < namelen ; i++)
+	dog->name[i] = name[i];
+
 	dog->age = age;
-	dog->owner = owner;
+
+	dog->owner = malloc(ownerlen * sizeof(dog->owner));
+	if (dog == NULL)
+		return (NULL);
+	for (i = 0 ; i < ownerlen ; i++)
+		dog->owner[i] = owner[i];
 	return (dog);
 }

779901245740202cecc8808193afd56e3b295202
ispdls <is.pdls90@gmail.com>
2023-11-29 21:35:20 -0400

    function that returns the number of elements in a linked list_t list.

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * list_len - unction that returns the number of elements in a linked list_t list
+ * list_len - unction that returns the number of elements in a linked list_t
  * @h: linked list to print
  * Return: number of nodes in the list
  */

4b3f2b3d31fd64cb717ade8083a86e359bb5b59c
ispdls <is.pdls90@gmail.com>
2023-12-10 23:40:30 -0400

    function that deletes a hash table.

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -7,33 +7,26 @@
   */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
+	hash_node_t *curr = NULL;
+	unsigned long int i = 0;
 
 	if (ht == NULL)
 		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			printf("First for loop\n");
 			while (ht->array[i])
 			{
-				printf("Freeing\n");
-				tmp = ht->array[i];
-				free(tmp->key);
-				tmp->key = NULL;
-				free(tmp->value);
-				tmp->value = NULL;
+				curr = ht->array[i];
+				free(curr->key);
+				free(curr->value);
 				ht->array[i] = ht->array[i]->next;
-				free(tmp);
-				tmp = NULL;
+				free(curr);
 			}
 		}
 	}
 	free(ht->array);
-	ht->array = NULL;
 	free(ht);
-	ht = NULL;
-	printf("Leaving\n");
 }

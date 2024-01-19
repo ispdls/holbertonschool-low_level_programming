356198130ef87b19459c81148e8d6a5e7b9c22b8
ispdls <is.pdls90@gmail.com>
2024-01-19 10:58:11 -0400

    search

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef MSEARCH_ALGOS_H
+#define MSEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+int linear_search(int *array, size_t size, int value);
+
+#endif

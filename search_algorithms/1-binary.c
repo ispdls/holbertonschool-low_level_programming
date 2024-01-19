53704ce530aba3a30c8d170295135a238917864a
ispdls <is.pdls90@gmail.com>
2024-01-19 14:32:29 -0400

    Binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located, or -1 if value is not present
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1, mid;
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		mid = (low + high) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
